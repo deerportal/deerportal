a2a7a0578eaa3399b7a9954b3a3c9320c8fb8cbd
bluszcz <bluszcz@bluszcz.net>
2019-10-10 19:20:35 +0200

    Snap is working with sound :deer: :-)

--- src/filetools.h
+++ src/filetools.h
@@ -1,6 +1,21 @@
 #ifndef FILETOOLS_H
 #define FILETOOLS_H
-static std::string get_full_path(std::string)
+
+#include <iostream>
+#include <string>
+
+static std::string get_full_path(std::string path)
 {
+	std::string new_path;
+	if ( getenv ( "DP_DIR" ) != NULL )
+	{
+		std::cout << "PATH " << new_path;
+		new_path = std::string(getenv("DP_DIR")) + path ;
+		std::cout << "NEW_PATH " << new_path << "\n";
+		return new_path;
+	}
+		std::cout << "PATH " << path << "\n";
+
+	return path;
 }
 #endif // FILETOOLS_H
