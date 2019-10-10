9f1336974cef488dbefbf606144b84d0442aa74a
bluszcz <bluszcz@bluszcz.net>
2019-10-10 20:31:55 +0200

    Less debug

--- src/filetools.h
+++ src/filetools.h
@@ -6,16 +6,12 @@
 
 static std::string get_full_path(std::string path)
 {
-	std::string new_path;
-	if ( getenv ( "DP_DIR" ) != NULL )
-	{
-		std::cout << "PATH " << new_path;
-		new_path = std::string(getenv("DP_DIR")) + path ;
-		std::cout << "NEW_PATH " << new_path << "\n";
-		return new_path;
-	}
-		std::cout << "PATH " << path << "\n";
-
-	return path;
+    std::string new_path;
+    if ( getenv ( "DP_DIR" ) != NULL )
+    {
+        new_path = std::string(getenv("DP_DIR")) + path ;
+        return new_path;
+    }
+    return path;
 }
 #endif // FILETOOLS_H
