3f433727b1e87dc5b09096268bc1405b977dceca
Rafal Zawadzki <bluszcz@bluszcz.net>
2023-02-10 16:59:31 +0100

    Added cpp 17

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,20 @@
 
 #include "game.h"
 #define _CRT_SECURE_NO_WARNINGS
+#include <filesystem>
 
 using namespace std;
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::filesystem::current_path;
+
 
 int main(int argc,      // Number of strings in array argv
          char *argv[]   // Array of command-line argument strings
          )
 {
-    
+    cout << "Current working directory: " << current_path() << endl;
+
     if (argc>1)
     {
         std::string commandMe;
